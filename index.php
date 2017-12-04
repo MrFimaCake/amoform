89d19dc909ac8a1ce25840f144a31fe8d39903af
George Korzh <mrcake@Georges-MacBook-Pro.local>
2017-12-05 01:04:38 +0200

    less dirtyness

--- index.php
+++ index.php
@@ -1,17 +1,9 @@
 <?php
 
-phpinfo();
-die;
-
-
 require (dirname(__FILE__)) . '/autoload.php';
 
 $config = require (dirname(__FILE__)) . '/config.php';
 
-//include 'contact_form.php';
 \amocrm\Credentials::setCredentials($config);
 
 echo \amocrm\AmoCrm::createDeal();
-
-//echo '<pre>';
-//die(var_dump(\amocrm\ApiClient::callApi()));
